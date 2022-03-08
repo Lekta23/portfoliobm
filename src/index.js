9f784630fcaad5f930580161e1eb88427518090d
Lekta23 <48383181+Lekta23@users.noreply.github.com>
2022-03-08 10:05:37 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // Import the functions you need from the SDKs you need
+// import {
+//     initializeApp
+// } from "firebase/app";
+// import {
+//     getAnalytics
+// } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDR1WBsXDKhHFIzVd8tkl_tI_X1wv2C1l8",
+//     authDomain: "boidin-mathieu.firebaseapp.com",
+//     projectId: "boidin-mathieu",
+//     storageBucket: "boidin-mathieu.appspot.com",
+//     messagingSenderId: "338249359500",
+//     appId: "1:338249359500:web:6cee8c9bd39c5f97f89ba9",
+//     measurementId: "G-H5MWRQY74M"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+ReactDOM.render( < App / > , document.getElementById('root'));
+registerServiceWorker();
